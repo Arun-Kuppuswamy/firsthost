27863a60c502cb05a8024bacb20ae9fc97853ac3
arun <arun.ayappa92@gmail.com>
2022-09-03 20:42:53 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDie9I7GEqRrcxYlyPL0YWr5KUn52sCzQE",
+    authDomain: "challengr-91e51.firebaseapp.com",
+    projectId: "challengr-91e51",
+    storageBucket: "challengr-91e51.appspot.com",
+    messagingSenderId: "17177954081",
+    appId: "1:17177954081:web:45a532b58313f924d0e060",
+    measurementId: "G-8Q054KZ1EN"
+};
